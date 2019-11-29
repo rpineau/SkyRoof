01aca5fe194b8fc806779f40401ad5e789cac02d
Rodolphe Pineau <pineau@rti-zone.org>
2019-11-28 16:32:52 -0800

    Fix macOS Xcode project, fix .gitignore, added notarize script

--- installer/build.sh
+++ installer/build.sh
@@ -6,12 +6,15 @@ cp "../SkyRoof.png" ROOT/tmp/SkyRoof_X2/
 cp "../domelist SkyRoof.txt" ROOT/tmp/SkyRoof_X2/
 cp "../build/Release/libSkyRoof.dylib" ROOT/tmp/SkyRoof_X2/
 
+PACKAGE_NAME="DomePro_X2.pkg"
+BUNDLE_NAME="org.rti-zone.SkyRoofX2"
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.SkyRoof_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 SkyRoof_X2.pkg
-pkgutil --check-signature ./SkyRoof_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.SkyRoof_X2 --scripts Scripts --version 1.0 SkyRoof_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
